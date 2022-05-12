0d61eeafb9b344f01ac04a6b43ab14eb5af50eb4
Hitesh Verma <hitesh.verma2711@gmail.com>
2022-05-12 18:08:37 +0530

    created file structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import MockmanEs from "mockman-js";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import logo from "./logo.png";
+import { Home, NotFound } from "./pages";
 
 function App() {
   return (
-      <Routes>
-        <Route path="mock" element={<MockmanEs />} />
-      </Routes>
+    <Routes>
+      <Route path="mock" element={<MockmanEs />} />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Footer, NavBar } from "../../components";
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+      
+      <Footer />
+    </>
+  );
+}
+
+export { Home };
